492474d4d5cf469e346835de726637505f19feb9
Jan Haans <jan.haans@gmail.com>
2020-12-12 21:36:15 +0100

    Finished store API

--- section3/app.py
+++ section3/app.py
@@ -25,7 +25,10 @@ def create_store():
 #GET /store/<string:name>   Get store with name = name
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'error': f'store with name {name} is not found'})
 
 #GET /store     Get list of all stores
 @app.route('/store')
@@ -35,12 +38,20 @@ def get_stores():
 #POST /store/<sting:name>/item  data:{name:,price:}     Create new item for store name = name
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            request_data = request.get_json()
+            store['items'].append({'name': request_data['name'], 'price': request_data['price']})
+            return jsonify({'message': f"item {request_data['name']} has been added to store {name}"})
+    return jsonify({'error': f'store with name {name} is not found'})
 
 #GET /store/<string:name>/item  Get all items of store with name = name
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'error': f'store with name {name} is not found'})
 
 
 app.run(host='0.0.0.0', port=5000)
