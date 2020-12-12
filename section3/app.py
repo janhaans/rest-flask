0da9d3409de7c87c546b1a250b6bd15fd36a1915
Jan Haans <jan.haans@gmail.com>
2020-12-12 16:46:13 +0100

    Store API - POST /store

--- section3/app.py
+++ section3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -14,7 +14,13 @@ stores = [
 #POST /store data:{name:}   Create new store with a name
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 #GET /store/<string:name>   Get store with name = name
 @app.route('/store/<string:name>')
