a4c07a321f08bd534e2a5b241a7e47f0341bf2b2
Jan Haans <jan.haans@gmail.com>
2020-12-16 21:21:35 +0100

    Finished POST of resource item

--- section4/app.py
+++ section4/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource #, reqparse
+from flask_restful import Api, Resource, reqparse
 #from flask_jwt import JWT, jwt_required
 
 app = Flask(__name__)
@@ -8,13 +8,16 @@ api = Api(app)
 items=[]
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help='Price is required')
+
     def get(self, name):
         pass
 
     def post(self, name):
         if next(filter(lambda x: x['name']== name, items), None):
-            return {'message': f'Item {name} is already in the list'}, 409
-        data = request.get_json()
+            return {'message': f'Item {name} is already in the list'}, 400
+        data = Item.parser.parse_args()
         new_item = {'name': name, 'price': data['price']}
         items.append(new_item)
         return new_item, 201
